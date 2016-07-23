2b1047c934aebf1bea0f100effd8cf33713059f5
shellyb985 <shellyb985@gmail.com>
2016-07-23 14:20:19 +0530

    Created Search Bar Backend

---
+++ SearchController.swift
@@ -0,0 +1,13 @@
+//
+//  SearchController.swift
+//  AppyStoreApplication
+//
+//  Created by BridgeIt on 23/07/16.
+//  Copyright © 2016 bridgelabz. All rights reserved.
+//
+
+import UIKit
+
+class SearchController: NSObject {
+
+}
---
+++ SearchViewControllerProtocol.swift
@@ -0,0 +1,13 @@
+//
+//  SearchViewControllerProtocol.swift
+//  AppyStoreApplication
+//
+//  Created by BridgeIt on 23/07/16.
+//  Copyright © 2016 bridgelabz. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewControllerProtocol: NSObject {
+
+}
