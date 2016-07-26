a9931412202a6dbc7444d3d3135a6608f0a4e4db
shellyb985 <shellyb985@gmail.com>
2016-07-26 10:50:53 +0530

    main story board removed from git ignore

---
+++ CollectionReusableView.swift
@@ -0,0 +1,13 @@
+//
+//  CollectionReusableView.swift
+//  AppyStoreApplication
+//
+//  Created by BridgeIt on 26/07/16.
+//  Copyright © 2016 bridgelabz. All rights reserved.
+//
+
+import UIKit
+
+class CollectionReusableView: UICollectionReusableView {
+        
+}
